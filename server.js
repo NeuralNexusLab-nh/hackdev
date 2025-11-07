94a6f5b1cb2f20f2d29612d24e2ec110d763bf95
NeuralNexusLab <neuralnexuslab@hotmail.com>
2025-11-08 07:14:29 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,55 +1,89 @@
 const express = require("fs");
-const cookie = require("cookie-parser");
 const path = require("path");
+const express = require("express");
 const app = express();
 
-app.use(cookie());
 app.set("trust proxy", true);
 
+app.use((req, res, next) => {
+  res.set("X-Team", "NetHacker");
+  res.set("X-HTTP-REQUEST-ID", Math.floor(Math.random() * 1000) * Math.floor(Math.random() * 1000))
+  next();
+});
+
 app.get("/", (req, res) => {
-  res.send(`<!doctype html><html><head><title>hackdev</title></head><body><h1>Hack Developer Tools</h1><br><a href="https://nethacker.cloud">Offical Website</a></body></html>`);
-});
-
-app.get("/setCookie", (req, res) => {
-  const key = req.query.key;
-  const value = req.query.value;
-
-  res.cookie(key, value);
-  res.send("set cookie success");
-});
-
-app.get("/set-cookie", (req, res) => {
-  const key = req.query.key;
-  const value = req.query.value;
-
-  res.cookie(key, value);
-  res.send("set cookie success");
-});
-
-app.get("/cookie", (req, res) => {
-  res.send(req.cookies);
+  res.send(`<h1>HackDev</h1>`);
 });
 
 app.get("/ip", (req, res) => {
   res.send(req.ip);
 });
 
-app.get("/headers", (req, res) => {
-  res.send(req.headers)
+app.get("/ua", (req, res) => {
+  res.send(req.headers["user-agent"]);
 });
 
-app.get("/register/:name", (req, res) => {
-  var registers = JSON.parse(fs.readFileSync("./register.json", "utf8"));
-  if (registers[req.params.name]) {
+app.get("/headers", (req, res) => {
+  res.send(req.headers);
+});
+
+const fs = require("fs");
+const express = require("express");
+const app = express();
+
+app.get("/dh/register/:id", (req, res) => {
+  let dh = JSON.parse(fs.readFileSync("./dh.json", "utf8"));
+  if (dh[req.params.id]) {
     res.send("ID already exists");
   } else {
-    fs.writeFile("./register.json", JSON.stringify(registers.push(req.params.name)), (err) => {
-      res.send("ID create success");
+    dh[req.params.id] = {
+      ip: " ",
+      ua: " ",
+      time: " ",
+      os: " ",
+      mobile: " ",
+      headers: " "
+    };
+    fs.writeFile("./dh.json", JSON.stringify(dh, null, 2), (err) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send("Error writing file");
+      } else {
+        res.send("ID create success");
+      }
     });
   }
 });
 
-app.get("/id/:id", (req, res) => {
-  var registers = JSON.parse(fs.readFileSync("./register.json"));
-  if (registers[req.params.id]) {
-    var data = fs.readFileSync(
+
+app.get("/dh/:id", (req, res) => {
+  var dh = JSON.parse(fs.readFileSync("./dh.json"));
+  if (dh[req.params.id]) {
+    dh[req.params.id].ip = req.ip;
+    dh[req.params.id].ua = req.headers["sec-ch-ua"];
+    dh[req.params.id].time = new Date();
+    dh[req.params.id].os = req.headers["sec-ch-ua-platform"];
+    dh[req.params.id].mobile = req.headers["sec-ch-ua-mobile"];
+    dh[req.params.id].headers = req.headers;
+    dh = JSON.stringify(dh);
+    fs.writeFile("./dh.json", dh, (err) => {
+      res.send("SUCCESS");
+    });
+  } else {
+    res.status(404).send("ID not found");
+  }
+});
+
+app.get("/xss.js", (req, res) => {
+  res.sendFile(path.join(__dirname, "xss.js"));
+});
+
+app.get("/return", (req, res) => {
+  res.type("text/plain").send(req.query.text);
+});
+
+app.get("/fetch", (req, res) => {
+  fetch(req.query.url).then(res => res.text()).then((data) => { res.type("text/plain").send(data); });
+});
+
+app.listen(process.env.PORT);
